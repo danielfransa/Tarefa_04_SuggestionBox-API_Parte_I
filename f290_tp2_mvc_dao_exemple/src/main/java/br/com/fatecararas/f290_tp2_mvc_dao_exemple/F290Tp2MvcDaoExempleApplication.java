0779765441b487bd641960a02ecacd62c241eb5a
Daniel França <102123924+danielfransa@users.noreply.github.com>
2023-05-19 22:02:34 -0300

    Add files via upload

---
+++ f290_tp2_mvc_dao_exemple/src/main/java/br/com/fatecararas/f290_tp2_mvc_dao_exemple/F290Tp2MvcDaoExempleApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatecararas.f290_tp2_mvc_dao_exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F290Tp2MvcDaoExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F290Tp2MvcDaoExempleApplication.class, args);
+	}
+
+}
---
+++ f290_tp2_mvc_dao_exemple/src/test/java/br/com/fatecararas/f290_tp2_mvc_dao_exemple/F290Tp2MvcDaoExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatecararas.f290_tp2_mvc_dao_exemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F290Tp2MvcDaoExempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
